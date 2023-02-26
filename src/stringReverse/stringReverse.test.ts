a962b4f75955b33dcacaf04f8aa296c3d2bdd2fe
dinesh1042 <dineshram1042002@gmail.com>
2023-02-26 11:32:48 +0530

    Add String Reverse Test Cases

--- src/stringReverse/stringReverse.test.ts
+++ src/stringReverse/stringReverse.test.ts
@@ -4,4 +4,12 @@ describe('String Reverse', () => {
   test('Abc should Be cbA', () => {
     expect(stringReverse('Abc')).toBe('cbA');
   });
+
+  test('Hello World! should Be !dlroW olleH', () => {
+    expect(stringReverse('Hello World!')).toBe('!dlroW olleH');
+  });
+
+  test('String Reverse should Be esreveR gnirtS', () => {
+    expect(stringReverse('String Reverse')).toBe('esreveR gnirtS');
+  });
 });
